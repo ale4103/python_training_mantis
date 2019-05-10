378d4dafbba26b63fb4ae5ce4ed65c6230fb24df
Alexander Ivanov4 <alexander_ivanov4@epam.com>
2019-05-10 11:22:59 +0300

    test_login was added

--- fixture/application.py
+++ fixture/application.py
@@ -1,8 +1,6 @@
 #from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium import webdriver
 from fixture.session import SessionHelper
-from fixture.group import GroupHelper
-from fixture.contact import ContactHelper
 
 class Application():
 
@@ -17,8 +15,6 @@ class Application():
             raise ValueError("Unrecognized browser %s" % browser)
         #self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
-        self.group = GroupHelper(self)
-        self.contact = ContactHelper(self)
         self.base_url = base_url
 
     def is_valid(self):
