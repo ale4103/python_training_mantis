f7507d70dad34dafa6f2f00744898638b0918561
alexander.ivanov <ale4103@gmail.com>
2019-05-13 14:45:03 +0300

    small fixes

--- fixture/soap.py
+++ fixture/soap.py
@@ -8,7 +8,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.19/api/soap/mantisconnect.php?wsdl")
+        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
