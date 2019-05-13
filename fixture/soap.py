ca2b8d2fbf2602695815401576661f5dcff7005f
alexander.ivanov <ale4103@gmail.com>
2019-05-13 14:30:53 +0300

    test_add_project and test_del_project are inplemented via soap

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,5 +1,6 @@
 from suds.client import Client
 from suds import WebFault
+from model.project import Project
 
 class SoapHelper:
 
@@ -13,3 +14,15 @@ class SoapHelper:
             return True
         except WebFault:
             return False
+
+    def get_project_from_soap(self):
+        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        test = client.service.mc_projects_get_user_accessible('administrator', 'root')
+        list = []
+        for i in test:
+            id = i.id
+            name = i.name
+            description = i.description
+            project_list = Project(id=id, name=name, description=description)
+            list.append(project_list)
+        return list
--- test/test_del_project.py
+++ test/test_del_project.py
@@ -5,12 +5,14 @@ import string
 def test_del_project(app):
     app.session.login("administrator", "root")
     app.project.open_project_page()
-    old_projects = app.project.get_project_list()
     if len(old_projects) == 0:
         app.project.create_project(Project(name=random_string("Project_", 10)))
+    # old_projects = app.project.get_project_list()
+    old_projects = app.soap.get_project_from_soap()
     deleted_project = random.choice(old_projects)
     app.project.delete_project(deleted_project.name)
-    new_projects = app.project.get_project_list()
+    #new_projects = app.project.get_project_list()
+    new_projects = app.soap.get_project_from_soap()
     assert len(old_projects) - 1 == len(new_projects)
     old_projects.remove(deleted_project)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
